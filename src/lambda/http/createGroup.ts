3148ae7a517be877e133ae258efed6011e60985e
Matthew Eddy <mattheweddy@Matthews-Air.attlocal.net>
2021-05-10 11:54:55 -0500

    added createGroup function and udated serverless.yml

---
+++ src/lambda/http/createGroup.ts
@@ -0,0 +1,33 @@
+import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import 'source-map-support/register';
+import * as AWS from 'aws-sdk';
+import * as  uuid from  'uuid';
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+const groupsTable = process.env.GROUPS_TABLE;
+
+export const createGroup: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent) : Promise <APIGatewayProxyResult> => {
+    console.log('Processing event:', event);
+    const itemId = uuid.v4();
+
+    const parseBody = JSON.parse(event.body);
+
+    const newItem = {
+        id: itemId,
+        ...parseBody
+    };
+
+    await docClient.put({
+        TableName: groupsTable,
+        Item: newItem
+    }).promise();
+
+    return {
+        statusCode: 201,
+        headers: {
+            "Access-Control-Allow-Origin": "*"
+        },
+        body: JSON.stringify({ newItem })
+    };
+
+}
